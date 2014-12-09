daa8f566481d9d06a667fcba601c7abfc4f4e4bb
Hao Tan <ihntee@gmail.com>
2014-12-09 19:22:26 +0800

    modify config

--- config.py
+++ config.py
@@ -1,12 +1,5 @@
 # edit here and fill your own username and password
-<<<<<<< HEAD
 USERNAME = "user"
 PASSWORD = "pass"
-=======
-USERNAME = "htan"
-PASSWORD = "htan28@nlplab"
-# USERNAME = "prdu"
-# PASSWORD = "prdu24@nlplab"
->>>>>>> th
 
 FILENAME = "fileee"
